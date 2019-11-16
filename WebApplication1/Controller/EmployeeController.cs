cc49433dd0414774f1cac7152da061130078f1b9
ilay00 <agidelio@mail.ru>
2019-11-16 17:12:38 +0500

    1611

---
+++ WebApplication1/Controller/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Websais.Models;
+namespace Websais.Controller
+{
+    public class EmployeeController //:Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+                {
+                new EmployeeView
+                {
+                Id = 1,
+                FirstName = "Иван" ,
+                SurName = "Иванов" ,
+                Patronymic = "Иванович" ,
+                Age = 100
+                },
+                new EmployeeView
+                {
+                Id = 2,
+                FirstName = "Петор" ,
+                SurName = "Петров" ,
+                Patronymic = "Иванович" ,
+                Age = 100
+                }
+                };
+       /* public IActionResult Index()
+        {
+            return View(_employees);
+        }*/
+    }
+}
---
+++ WebApplication1/Controller/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Websais.Models;
+
+namespace Websaite.Controler
+{
+    public class HomeController : Controller
+    {
+       
+            public HomeController() { }
+
+                public IActionResult blog() => View();
+             //   public IActionResult blog-single() => View();
+                public IActionResult  cart() => View();
+                public IActionResult checkout() => View();
+           //     public IActionResult contact-us() => View();
+            public IActionResult index() => View();
+         //   public IActionResult product-details() => View();
+            public IActionResult shop() => View();
+
+
+
+
+
+    }
+}
