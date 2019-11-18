bfe233cb997dd70b0eaefdf525e431db82666b57
ilay00 <agidelio@mail.ru>
2019-11-19 00:06:44 +0500

    Добавил предстовления Employees/index

---
+++ WebAppSATES/WebAppSATES/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppSATES.Models;
+
+namespace WebAppSATES.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly List<EmployeeView> __Employees = new List<EmployeeView>
+            {
+            new EmployeeView
+            {
+
+             Id=1,
+             FirstName="Василий",
+             SecondName="Иванов",
+             Patronymic="Иванович",
+             Age=52
+                     },
+
+             new EmployeeView
+            {
+
+             Id=1,
+             FirstName="Иван",
+            SecondName="Иванов",
+             Patronymic="Иванович",
+             Age=22
+                     }
+
+
+            };
+        public IActionResult Index()
+        {
+            ViewBag.SomeData = "Hello World!";
+            ViewData["Test"] = "TestData";
+
+            return View();
+        }
+    }
+}
